67cfe114aacb2c8dda44353617a74765f438049c
Matheus Moura <matheuslnmoura@gmail.com>
2022-04-04 17:55:32 -0300

    History Page layout implemented - simplified version

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -12,7 +12,10 @@ export default function History() {
     return(
         <HistoryPage>
             <Header />
-            <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+            <HistoryContainer >
+                <h1>Histórico</h1>
+                <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+            </HistoryContainer>
             <Footer />
         </HistoryPage>
     )
@@ -21,6 +24,29 @@ export default function History() {
 const HistoryPage = styled.section`
     width: 100%;
     height: calc(100vh - 140px);
-    background-color: #ccc;
+    background-color: #E5E5E5;
 
+`
+
+const HistoryContainer = styled.div`
+    width: 90%;
+    margin:auto;
+
+    h1 {
+        color: var(--main-color);
+        font-style: normal;
+        font-weight: 400;
+        font-size: 22.976px;
+        line-height: 29px;
+        margin-top: 28px;
+        margin-bottom: 17px;
+    }
+
+    p {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 17.976px;
+        line-height: 22px;
+        color: #666666;
+    }
 `
